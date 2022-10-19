984d25bd779163d6653ec4bcfd45321ffa52e710
jmcheon <yosoi1225@gmail.com>
2022-10-20 01:40:50 +0200

    Add: fecth() asking login

--- mvc_nodejs/app/src/public/js/home/login.js
+++ mvc_nodejs/app/src/public/js/home/login.js
@@ -2,11 +2,20 @@ const id = document.querySelector("#id");
 const password = document.querySelector("#password");
 const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", login);
+
+function login() {
   const request = {
     id: id.value,
     password: password.value,
   };
-});
-
-console.log(request);
+  // console.log(request);
+  // console.log(JSON.stringify(request));
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(request),
+  });
+}
